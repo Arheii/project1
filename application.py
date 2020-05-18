d3242ea04259a053b42883ec7a7e16103c48d47d
Arheii <arheyy@gmail.com>
2020-05-18 21:45:48 +0000

    add login page; finish register, login and logout section in application.py

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -31,18 +32,37 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # Forget any user id
     session.clear()
     if request.method == "GET":
-        return render_template("login.html")
-    else:
-        if not request.form.get("username"):
-            return render_template("index.html", 404)
+        return render_template("login.html", message="")
+
+    # Request form
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not all((email, password)):
+        return render_template("login.html",  message="All fields must be filled"), 406
+
+    # Check email is exist
+    ans = db.execute("SELECT id, username, pass_hash FROM users WHERE email = :email",
+                             {"email": email}).fetchone()
+    if ans is None:
+        print(f"ANS = {ans}")
+        return render_template("login.html",  message="User not found")
+
+    # Check password
+    user_id, username, pass_hash = ans
+    if not check_password_hash(pass_hash, password):
+        return render_template("login.html",  message="Incorrect password")
+
+    # Finish log in, write user to session
+    session['user_id'], session['username'] = user_id, username
+    return render_template("index.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def registration():
     session.clear()
-
     if request.method == "GET":
         return render_template("register.html", message="")
 
@@ -51,5 +71,26 @@ def registration():
     username = request.form.get("username")
     if not all((email, password, username)):
         return render_template("register.html",  message="All fields must be filled"), 406
-    return "TODO"
 
+    # Check lenght of data
+    if len(email) > 50 or len(password) > 128 or len(username) > 128:
+        return render_template("register.html",  message="Too long, be shorter ^^"), 406
+
+    # Email is exist
+    if db.execute("SELECT id FROM users WHERE email = :email", {"email": email}).rowcount:
+        return render_template("register.html",  message=f"{email} is busy, mb is it you?"), 406
+
+    pass_hash = generate_password_hash(password)
+    db.execute("INSERT INTO users (email, pass_hash, username) VALUES (:email, :pass_hash, :username)",
+            {"email": email, "pass_hash": pass_hash, "username": username})
+    db.commit()
+    session['user_id'] = db.execute("SELECT id FROM users WHERE email = :email",
+                             {"email": email}).fetchone()[0]
+    session['username'] = username
+    return render_template("index.html")
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session.clear()
+    return render_template("index.html")
